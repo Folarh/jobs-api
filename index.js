d55dd041a7f6500d4fdc624843a889fbdf07b634
folarin <folarintaiye@gmail.com>
2023-07-08 07:28:20 +0100

    new job api

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config;
+const express = require("express");
+const app = express();
+
+require("express-async-error");
+//connectDb
+const connectDB = require("./database/connect");
+
+//routers
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+
+//error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+//other packages
+
+//routes
+app.use("api/v1/auth", authRouter);
+app.use("api/v1/jobs", jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.Port || 8000;
+
+const start = async () => {
+  try {
+    //connect to database
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ middleware/error-handler.js
@@ -0,0 +1,11 @@
+const { CustomApIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof CustomApIError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+};
+
+module.exports = errorHandlerMiddleware;
